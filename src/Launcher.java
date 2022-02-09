5e9430564888e5d26eee1806f3aebca21a2a718d
nmolina42 <58724725+nicolaas91@users.noreply.github.com>
2022-02-09 15:31:39 +0100

    2eme partie

---
+++ src/Launcher.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        Scanner in = new Scanner(System.in);
+        String msg = in.nextLine();
+        if (!msg.equals("quit")){
+            System.out.println("Unknow command");
+        }
+    }
+}
