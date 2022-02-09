7aafde8e6aa1a03a423ed7bb00a5f619a46ec7b7
nmolina42 <58724725+nicolaas91@users.noreply.github.com>
2022-02-09 15:49:06 +0100

    fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,22 @@ public class Launcher {
         System.out.println("Welcome");
         Scanner in = new Scanner(System.in);
         String msg = in.nextLine();
-        while (!msg.equals("quit")){
+        while (!msg.equals("quit") && !msg.equals("fibo")) {
             System.out.println("Unknown command");
             msg = in.nextLine();
+        }
+        if (msg.equals("fibo")) {
+            String nb = in.nextLine();
+            int n = Integer.parseInt(nb);
+            int f1 = 1;
+            int f2 = 1;
+            int fn = n;
+            for (int i = 2; i < n; i++){
+                fn = f1 + f2;
+                f1 = f2;
+                f2 = fn;
+            }
+            System.out.println(fn);
         }
     }
 }
