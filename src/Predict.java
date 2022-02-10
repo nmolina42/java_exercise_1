056ea0cf77340e59a3854cb107f2f07c4786c600
nmolina42 <58724725+nicolaas91@users.noreply.github.com>
2022-02-10 17:03:57 +0100

    test part7

--- src/Predict.java
+++ src/Predict.java
@@ -7,12 +7,32 @@ import java.util.Scanner;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
-
 public class Predict implements src.Command {
 
+
+    private static class Word {
+        private final String word;
+        private final Map<String, Integer> occurences = new HashMap<>();
+
+        public Word(String word) {
+            this.word = word;
+        }
+
+        public void putFollower(String w) {
+            this.occurences.put(w, this.occurences.getOrDefault(w, 0) + 1);
+        }
+
+        public String predict() {
+            if (occurences.isEmpty())
+                return null;
+
+            var i = Collections.max(occurences.values());
+
+            var list = this.occurences.keySet().stream().filter(k -> occurences.get(k).equals(i)).toList();
+
+            return list.get(0);
+        }
+    }
 
     @Override
     public String name() {
@@ -24,8 +44,9 @@ public class Predict implements src.Command {
         System.out.println("Veuillez donner le chemin du fichier à lire ?");
         String pathStr = sc.nextLine();
         Path path = Paths.get(pathStr);
+        String fd;
         try {
-            String fd = java.nio.file.Files.readString(path);
+            fd = java.nio.file.Files.readString(path);
             System.out.println("Veuillez maintenant entrer un mot :");
             var word = sc.nextLine().toLowerCase();
             fd = fd.toLowerCase(Locale.ROOT);
@@ -36,47 +57,32 @@ public class Predict implements src.Command {
                 System.out.println("Le mot n'est dans le fichier");
                 return false;
             }
-            String[] listWord = fd.split(" ");
-            Map<String, String> wordsFreq = new HashMap<String, String>();
-            for (String words : listWord) {
-                if (wordsFreq.get(words) == null) {
-                    wordsFreq.put(words, getNextFreq(words, listWord));
-                }
-            }
-            System.out.print(word);
-            for (int i = 0; i < 19; i++) {
-                System.out.print(" " + wordsFreq.get(word));
-                word = wordsFreq.get(word);
+            Map<String, Word> words = new HashMap<>();
 
+            var lastWord = Arrays.stream(fd.split(" "))
+                    .filter(s -> !s.isBlank())
+                    .reduce("", (prev, next) -> {
+                        if (!prev.isBlank()) {
+                            words.putIfAbsent(prev, new Word(prev));
+                            words.get(prev).putFollower(next);
+                        }
+                        return next;
+                    });
+
+            words.putIfAbsent(lastWord, new Word(lastWord));
+
+            var sentence = new ArrayList<>(List.of(word));
+            while (sentence.size() < 20) {
+                var nextWord = words.get(sentence.get(sentence.size() - 1)).predict();
+                if (nextWord == null)
+                    break;
+                sentence.add(nextWord);
+                System.out.println(String.join(" ", sentence));
             }
+            return false;
+        } catch (Exception e) {
+            System.err.println("Unreadable file: " + e.getMessage());
+            return false;
         }
-        catch(IOException e) {
-            System.out.println("Unreadable file: " + e.toString());
-        }
-        System.out.println("");
-        return false;
     }
-    public String getNextFreq(String word,String[] content){
-        ArrayList<String> tmp = new ArrayList<>();
-        for (int i = 0;i < content.length;i++){
-            if (content[i].equals(word) && i != content.length - 1){
-                tmp.add(content[i+1]);
-             }
-          }
-        Map<Object, Integer> frequencyMap = tmp.stream()
-                .collect(toMap(
-                        s -> s, // key is the word
-                        s -> 1, // value is 1
-                        Integer::sum));
-        List<Object> res = tmp.stream()
-                .sorted(comparing(frequencyMap::get).reversed()) // sort by descending frequency
-                .distinct() // take only unique values
-                .limit(1)   // take only the first 1
-                .collect(toList()); // put it in a returned list
-
-          if (res.size() != 0) {
-              return res.get(0).toString();
-          }
-         return "";
-      }
 }
